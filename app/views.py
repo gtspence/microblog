14ffc58fbbacdac3e7162a8d5bf883a073732bb5
gtspence <graemespence26@gmail.com>
2016-04-11 16:55:45 +0100

    After Part II

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	user = {'nickname': 'Graeme'} #fake user
+	posts = [	# fake array of posts
+		{
+			'author':{'nickname': 'Tom'},
+			'body': 'Beautiful day in San Francisco!'
+		},
+		{
+			'author':{'nickname': 'Jenni'},
+			'body': 'My husband is amazing!'
+		}
+	]
+	return render_template('index.html',
+							#title='Home',
+							user=user,
+							posts=posts)
