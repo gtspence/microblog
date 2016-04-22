c91ca8e1a8a3c72f95692b3e9395b28cc133e1c3
gtspence <graemespence26@gmail.com>
2016-04-22 09:48:31 +0100

    Part IV (ongoing)

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Graeme S'} #fake user
+	user = {'nickname': 'Graeme'} #fake user
 	posts = [	# fake array of posts
 		{
 			'author':{'nickname': 'Tom'},
@@ -16,6 +17,18 @@ def index():
 		}
 	]
 	return render_template('index.html',
-							#title='Home',
+							title='Home',
 							user=user,
 							posts=posts)
+							
+@app.route('/login', methods=['GET', 'POST']) #methods=POST allows form data stored
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+				(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',
+							title='Sign In',
+							form=form,
+							providers=app.config['OPENID_PROVIDERS'])
