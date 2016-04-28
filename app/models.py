0733c35aae48b8e8906a4c6cffdd99fc2ce10e1d
gtspence <graemespence26@gmail.com>
2016-04-28 16:30:00 +0100

    Part X

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
-from app import db
+from app import db, app
 from hashlib import md5
+
+import sys
+if sys.version_info >= (3, 0):	# Whoosh doesn't work with Python 3.0
+	enable_search = False
+else:
+	enable_search = True
+	import flask.ext.whooshalchemy as whooshalchemy
 
 followers = db.Table('followers',
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -74,6 +81,8 @@ class User(db.Model):
 
 		
 class Post(db.Model):
+	__searchable__ = ['body']
+	
 	id = db.Column(db.Integer, primary_key = True)
 	body = db.Column(db.String(140))
 	timestamp = db.Column(db.DateTime)
@@ -81,4 +90,10 @@ class Post(db.Model):
 	
 	def __repr__(self):
 		return '<Post %r>' % (self.body)
+
+
+if enable_search:
+	whooshalchemy.whoosh_index(app, Post)		
+
+
 	
